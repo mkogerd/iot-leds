d1830d30441e1565bd8e7bf3ff467e966c35d27b
root <mkogerd@gmail.com>
2016-09-24 18:39:37 +0000

    Minimize index.php code

--- index.php
+++ index.php
@@ -8,10 +8,11 @@
 
 <html>
 	<head>
-		<!-- Latest compiled and minified CSS -->
+
+		<!-- Latest compiled and minified Bootstrap CSS -->
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-		<!-- Import Jquery UI stylesheets -->
+		<!-- Latest compiled Jquery-UI CSS -->
 		<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css">
 
 		<!-- Custom style Jquery sliders to match colors -->
@@ -28,13 +29,10 @@
 			.slider[data-color="r"] .ui-slider-range { background: #ff0000; }
 			.slider[data-color="g"] .ui-slider-range { background: #00ff00; }
 			.slider[data-color="b"] .ui-slider-range { background: #0000ff; }
-			.input-group-addon {
-                                min-width: 180px;
-			}
-			.input-group-addon-slider {
-				width: 50%;
-			}
+			.input-group-addon { min-width: 180px; }
+			.input-group-addon-slider { width: 50%; }
 		</style>
+
 	</head>
 	<body> 	
 
@@ -42,50 +40,51 @@
 		<div class="panel panel1"><h1 id="title" style="">Control my lights!</h1></div>
 
 		<!-- Main Content -->
-		<form id="form1" class="container" method="get" action="form.php">
+		<div class="container">
 			<!-- Color Title -->
 			<div class="panel panel-default" style="margin:0;">
-				<div class="panel-heading" style="margin:0;"><h2>Color<h2></div>
+				<div class="panel-heading" style="margin:0;"><h2><b>Color</b><h2></div>
 			</div>
 
 			<!-- Red Control Fields -->
 			<div class="input-group input-group-lg">
 				<span class="input-group-addon"><b>Red</b></span>
-				<input type="text" id="r" name="r" class="form-control text-center" value="<?php echo $r; ?>"><br>
+				<input type="text" id="r" class="form-control text-center" value="<?php echo $r; ?>"><br>
 				<span class="input-group-addon input-group-addon-slider"><div id="rslider" class="slider" data-color="r"></div></span>
 			</div>
 
 			<!-- Green Control Fields -->
 			<div class="input-group input-group-lg">
 				<span class="input-group-addon"><b>Green</b></span>
-				<input type="text" id="g" name="g" class="form-control text-center" value="<?php echo $g; ?>"><br>
+				<input type="text" id="g" class="form-control text-center" value="<?php echo $g; ?>"><br>
 				<span class="input-group-addon input-group-addon-slider"><div id="gslider" class="slider" data-color="g"></div></span>
 			</div>
 
 			<!-- Blue Control Fields -->
 			<div class="input-group input-group-lg">
 				<span class="input-group-addon"><b>Blue</b></span>
-				<input type="text" id="b" name="b" class="form-control text-center" value="<?php echo $b; ?>"><br>
+				<input type="text" id="b" class="form-control text-center" value="<?php echo $b; ?>"><br>
 				<span class="input-group-addon input-group-addon-slider"><div id="bslider" class="slider" data-color="b"></div></span>
 			</div>
 
 			<!-- HTML5 Color-Picker Control Field  and Submit-->
 			<div class="panel panel-default" style="margin:0;">
 				<div class="panel-heading" style="margin:0;">
-					Color Value (#ffffff):<br>
-					<input type="color" id="color" name="color" value="<?php echo $colorString; ?>"><br>
-					<br>
-					<input id="submit" type="submit">
-					<br>
+					<form  method="get" action="form.php">
+						Color Value (#ffffff):<br>
+						<input type="color" id="color" name="color" value="<?php echo $colorString; ?>"><br>
+						<br>
+						<input id="submit" type="submit">
+						<br>
+					</form>
 				</div>
 			</div>
-			<div id="swatch" class="ui-widget-content ui-corner-all"></div>
 
-		</form>
+		</div>
 	</body>
 </html>
 
-<!-- Import Jquery Scripts -->
+<!-- Latest compiled Jquery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js" integrity="sha256-0YPKAwZP7Mp3ALMRVB2i8GXeEndvCq3eSl/WsAl1Ryk=" crossorigin="anonymous"></script>
 
@@ -116,7 +115,7 @@
 		$("#color").val("#"+hex);			// Update color-picker input color 
 	}
 
-	// Jquery JS
+	// Document ready functions
 	$(document).ready(function() {
 		
 		// Preload color values
@@ -155,35 +154,27 @@
 		// Update sliders from color picker
 		if (color_support) {	// Browsers that support color input type (Chrome, android devices, etc...)
 			$('#color').on('input', function() {
+				// Get individual color values
 				var r = parseInt(this.value.substring(1, 3), 16),
 				g = parseInt(this.value.substring(3, 5), 16),
 				b = parseInt(this.value.substring(5, 7), 16);
-				console.log("Red: : "+r+" Green: "+g+" Blue: "+b);
+				// Update sliders with values
 				$('#rslider').slider('value', r);
 				$('#gslider').slider('value', g);
 				$('#bslider').slider('value', b);
 			});
 		} else {		// Devices that don't support color input type (Safari, Crapple, etc...)
 			$('#color').on('blur', function() {
+				// Get individual color values
 				var r = parseInt(this.value.substring(1, 3), 16),
 				g = parseInt(this.value.substring(3, 5), 16),
 				b = parseInt(this.value.substring(5, 7), 16);
-				console.log("Red: : "+r+" Green: "+g+" Blue: "+b);
+				// Update sliders with values
 				$('#rslider').slider('value', r);
 				$('#gslider').slider('value', g);
 				$('#bslider').slider('value', b);
 			});
 		}
-
-		// Append JS slider values to GET request
-		$("#form1").on('submit',function(event){
-			var slideRed = $("<input>").attr("type", "hidden").attr("name", "slideRed").val($("#rslider").slider("value"));
-			var slideGreen = $("<input>").attr("type", "hidden").attr("name", "slideGreen").val($("#gslider").slider("value"));
-			var slideBlue = $("<input>").attr("type", "hidden").attr("name", "slideBlue").val($("#bslider").slider("value"));
-			$('#form1').append($(slideRed));
-			$('#form1').append($(slideGreen));
-			$('#form1').append($(slideBlue));
-		});
 	});
 
 </script>
